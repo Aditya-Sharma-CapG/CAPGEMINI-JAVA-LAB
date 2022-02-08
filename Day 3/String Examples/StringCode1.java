43d93fa162eca8305bf8d268faef5ea313e68393
Aditya Narayan Sharma <98949445+Aditya-Sharma-CapG@users.noreply.github.com>
2022-02-08 09:23:43 +0530

    Add files via upload

---
+++ Day 3/String Examples/StringCode1.java
@@ -0,0 +1,13 @@
+package com.aditya;
+
+public class StringCode1 {
+
+	public static void main(String[] args) {
+		
+		String greet = "Hello! World";
+		System.out.println("String:"+greet);
+		
+//		to print the length of the string
+		System.out.println(greet.length());
+	}
+}
