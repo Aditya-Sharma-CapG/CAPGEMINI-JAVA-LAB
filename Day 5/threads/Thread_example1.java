b103f4f06cccca3e56438166f5ac23542e9bcbc0
Aditya Narayan Sharma <98949445+Aditya-Sharma-CapG@users.noreply.github.com>
2022-02-09 14:46:23 +0530

    Add files via upload

---
+++ Day 5/threads/Thread_example1.java
@@ -0,0 +1,24 @@
+package com.threads;
+
+public class Thread_example1 implements Runnable{
+
+        @Override
+        public void run () {
+        }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread();
+        thread1.start();
+
+        try {
+            thread1.sleep(1000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread1.setPriority(1);
+        int priority = thread1.getPriority();
+        System.out.println(priority);
+        System.out.println("Thread Running");
+    }
+}
